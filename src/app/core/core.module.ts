a6653b02a58903b8052caa5b4ab732f3a848a85d
fr4nkdev <francisco.nicolas@outlook.com>
2020-11-04 13:07:13 -0600

    add layout admin

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,3 +1,7 @@
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -6,9 +10,42 @@ import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { FooterComponent } from './layouts/main-layout/footer/footer.component';
+import { HeaderComponent } from './layouts/main-layout/header/header.component';
+import { SidebarComponent } from './layouts/main-layout/sidebar/sidebar.component';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-  declarations: [MainLayoutComponent, AuthLayoutComponent, AdminLayoutComponent],
-  imports: [CommonModule, RouterModule, BrowserModule, BrowserAnimationsModule],
+  declarations: [
+    MainLayoutComponent,
+    AuthLayoutComponent,
+    AdminLayoutComponent,
+    FooterComponent,
+    HeaderComponent,
+    SidebarComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatDividerModule,
+    FlexLayoutModule,
+    MatCardModule,
+    MatPaginatorModule,
+    MatTableModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatListModule,
+  ],
 })
 export class CoreModule {}
--- src/app/core/layouts/main-layout/main-layout.component.ts
+++ src/app/core/layouts/main-layout/main-layout.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main-layout',
   templateUrl: './main-layout.component.html',
-  styleUrls: ['./main-layout.component.scss']
+  styleUrls: ['./main-layout.component.scss'],
 })
 export class MainLayoutComponent implements OnInit {
+  sideBarOpen = true;
+  menuItems = ['dashboard', 'verkäufe', 'bestellungen', 'kunden', 'produkte'];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit() {}
+
+  sideBarToggler() {
+    this.sideBarOpen = !this.sideBarOpen;
   }
-
 }
